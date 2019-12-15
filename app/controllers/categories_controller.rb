690c3675c312f21cd2037ff23d8fb80c53613e33
Rosy Arreola <rosy.arreola@yahoo.com>
2019-12-15 01:16:42 +0000

    fixing stuff

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,10 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  private
+  
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 end
